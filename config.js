9f2c9df905fdcc8afa0468cd15afdad714dbb42d
Kyianthe1st <84585853+Kyianthe1st@users.noreply.github.com>
2022-02-02 13:23:35 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: "",//ur bot token
+    prefix: "a!",//ur bot prefix 
+    COOLDOWN: "0",//cooldown for autosetup command must be a number
+    owner: ['']//Add your ids like ["123456789", "123456789"] So you can able to use eval command!
+}
